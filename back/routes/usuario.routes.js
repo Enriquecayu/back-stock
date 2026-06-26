21d041994434563f0ee31ed2a93028d79fb86105
Enrique <uwuadri2008@gmail.com>
2026-06-26 00:11:54 -0300

    cambio temporal

--- back/routes/usuario.routes.js
+++ back/routes/usuario.routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import { registrar, login } from '../controllers/usuario.controller.js';
 import { verificarToken, esAdministrador } from '../middlewares/auth.middleware.js';
+import Usuario from '../models/Usuario.js';
 
 const router = Router();
 
@@ -10,5 +11,17 @@ router.post('/registrar', verificarToken, esAdministrador, registrar);
 
 // 🔑 PÚBLICO: Permite el inicio de sesión para obtener el token de acceso
 router.post('/login', login);
+
+router.post('/registro-inicial', async (req, res) => {
+    try {
+        const cuenta = await Usuario.count();
+        if (cuenta > 0) return res.status(403).json({ error: "Ya existen usuarios, no puedes usar esta ruta." });
+
+        // Llamamos directamente al controlador 'registrar'
+        await registrar(req, res);
+    } catch (error) {
+        res.status(500).json({ error: "Error en el registro inicial" });
+    }
+});
 
 export default router;
